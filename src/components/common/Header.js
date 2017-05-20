68081a7d65daea65b0be8857f61fc891f92051f5
Santiago Ibarguen <sibarguenc@gmail.com>
2017-05-20 19:46:58 -0400

    Header component added

---
+++ src/components/common/Header.js
@@ -0,0 +1,38 @@
+//Import libraries Header component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+//Make component
+const Header = (props) => {
+
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+}
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#eeeeee',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20,
+    color: '#000'
+  }
+};
+
+
+//Make component available to other parts of application
+export  { Header };
