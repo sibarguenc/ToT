b998286bfa70f055a851878000facd9a6fd31d46
Santiago Ibarguen <sibarguenc@gmail.com>
2017-05-21 16:08:36 -0400

    Change status bar to white

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Font } from 'expo';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { Button } from './src/components/common';
 import { Router } from './Router';
+
 
 export default class App extends React.Component {
   state = {
@@ -20,6 +21,7 @@ async componentDidMount() {
 
 
   render() {
+    StatusBar.setBarStyle('light-content', true);
     return (
       <View style={styles.container}>
           {
